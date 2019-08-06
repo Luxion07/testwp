60cf9d3db6ec51b60bb1db8e0d5d0367baea4131
luxion <luxion0770@gmail.com>
2019-08-06 22:59:17 +0300

    fix js code (formating)

--- wp-content/themes/luxion/js/main.js
+++ wp-content/themes/luxion/js/main.js
@@ -2,76 +2,62 @@ jQuery(document).ready(function ($) {
 
     /* Search functional*/
 
-    var queryTaxArray = {},
-        departmentNewName = [],
-        locationNewName = [];
+    var queryTaxObject = {},
+        departmentArray = [],
+        locationArray = [];
 
     // filter by tabs and dropdowns
 
     $('.choice-list__label, .tabs-list__button').on('click', function () {
-
-        var i = 0;
+        var element = $(this);
 
         $(this).closest('li').toggleClass('checked'); // if click current list add checked className
 
-        let element = $(this);
-
         updateInput(element);
-
         dropdownSearch();
-
         liveSearch();
-
     });
 
     // filter by search input
 
     $('.jobs-filter-search__input').on('keyup', function () {
-
-        let searchKeywords = $(this).val();
+        var searchKeywords = $(this).val();
 
         liveSearch(searchKeywords);
 
-        if(searchKeywords ===  '') {
+        if (searchKeywords === '') {
             $('.jobs-filter-search__input-close').hide();
-        }else{
+        } else {
             $('.jobs-filter-search__input-close').show();
         }
-
     });
-
 
     // select all filter dropdowns
 
-    $('.choice-list-select-all').on('click', function(){
+    $('.choice-list-select-all').on('click', function () {
+        var choiceList = $(this).closest('.choice-list__menu');
 
-        let choiceList = $(this).closest('.choice-list__menu');
-        choiceList.find('.choice-list__item').each(function(){
+        choiceList.find('.choice-list__item').each(function () {
             $(this).addClass('checked');
             updateInput($(this));
-
         });
 
         dropdownSearch();
-
         liveSearch();
 
         $(this).hide();
         choiceList.find('.choice-list-deselect-all').show();
     });
 
+    $('.choice-list-deselect-all').on('click', function () {
+        var choiceList = $(this).closest('.choice-list__menu');
 
-    $('.choice-list-deselect-all').on('click', function(){
-
-        let choiceList = $(this).closest('.choice-list__menu');
-
-        choiceList.find('.choice-list__item').each(function(){
+        choiceList.find('.choice-list__item').each(function () {
             $(this).removeClass('checked');
             updateInput($(this));
         });
 
         dropdownSearch();
-
         liveSearch();
 
         $(this).hide();
@@ -81,38 +67,34 @@ jQuery(document).ready(function ($) {
     //live search function (ajax call)
 
     function liveSearch($keyword) {
-
-        let data = {
+        var data = {
             action: 'filter_jobs',
             searchData: $keyword,
-            taxData: queryTaxArray
+            taxData: queryTaxObject
         };
 
         $.post(MyAjax.ajaxurl, data, function (response) {
             $('.free-vacancies__items-wrap').html(response);// Set data to page as search result
             countVacancies();
         });
-
-
     }
 
-    function updateInput (element){
-
-        let filterDropdown = element.closest('.jobs-filter-search__dropdown-wrap'),
+    function updateInput(element) {
+        var filterDropdown = element.closest('.jobs-filter-search__dropdown-wrap'),
             filterDropdownName = filterDropdown.data('filter-category'); // get original name of field (const)
 
         labelName = element.find('.choice-list__label-name').text();
 
         if (filterDropdownName === 'All departments') {
-            var idx = $.inArray(labelName, departmentNewName);
+            var idx = $.inArray(labelName, departmentArray);
 
             if (idx === -1) {
-                departmentNewName.push(labelName);
+                departmentArray.push(labelName);
             } else {
-                departmentNewName.splice(idx, 1);
+                departmentArray.splice(idx, 1);
             }
 
-            let newDropdown = departmentNewName.sort().join(", ");
+            var newDropdown = departmentArray.sort().join(', ');
 
             if (newDropdown[0] === ',') {
                 newDropdown = newDropdown.substr(1);
@@ -125,15 +107,14 @@ jQuery(document).ready(function ($) {
             }
 
         } else if (filterDropdownName === 'All locations') {
-            var idx = $.inArray(labelName, locationNewName);
+            var idx = $.inArray(labelName, locationArray),
+                newLocation = locationArray.sort().join(', ');
 
             if (idx === -1) {
-                locationNewName.push(labelName);
+                locationArray.push(labelName);
             } else {
-                locationNewName.splice(idx, 1);
+                locationArray.splice(idx, 1);
             }
-
-            let newLocation = locationNewName.sort().join(", ");
 
             if (newLocation[0] === ',') {
                 newLocation = newLocation.substr(1);
@@ -144,37 +125,33 @@ jQuery(document).ready(function ($) {
             } else {
                 $("[data-filter-category='All locations'] .jobs-filter-search__dropdown").text("All locations");
             }
-
         }
+
     }
 
-    function dropdownSearch (){
-
+    function dropdownSearch() {
         var i = 0;
 
         $('.jobs-filter__row .checked').each(function () { // get all checked items
-
-            let termID = $(this).data('filter-termid'),
+            var termID = $(this).data('filter-termid'),
                 tax = $(this).data('filter-tax');
 
-            queryTaxArray[i] = {
+            queryTaxObject[i] = {
                 terms: termID,
                 field: 'id',
                 taxonomy: tax
             };
-
             i++;
         });
 
         if ($(".jobs-filter__row .checked")[0]) {
-
         } else {
-            queryTaxArray = {};
+            queryTaxObject = {};
         }
 
-        queryTaxArray['relation'] = 'OR'; // add relation for multiply filter posts
+        queryTaxObject['relation'] = 'OR'; // add relation for multiply filter posts
 
-        return queryTaxArray;
+        return queryTaxObject;
     }
 
     /* Other functional*/
@@ -194,62 +171,48 @@ jQuery(document).ready(function ($) {
     // count free vacancies
 
     function countVacancies() {
-
-        let freeVacancies = $('.open-vac .free-vacancy');
+        var freeVacancies = $('.open-vac .free-vacancy');
 
         $('.filter__row-headline-counter').text(freeVacancies.length);
-
     }
 
     countVacancies();
 
-
     // get height footer variable
 
-    let footerHeight = $('.footer').outerHeight();
+    var footerHeight = $('.footer').outerHeight();
+
     $('.career-description').css({'margin-bottom': footerHeight});
     $('.job-single__description').css({'margin-bottom': footerHeight});
 
-
     // mobile menu func
 
-    $('.close-menu').on('click', function (){
+    $('.close-menu').on('click', function () {
         $(this).closest('.mobile-menu').removeClass('active');
     });
 
     $('.mobile-btn .burger').on('click', function () {
-       $(this).closest('.main-header').find('.mobile-menu').addClass('active');
+        $(this).closest('.main-header').find('.mobile-menu').addClass('active');
     });
-
 
     // filter form delete input
 
     $('.jobs-filter-search__input-close').on('click', function () {
-
-       $(this).hide();
-
-       $(this).closest('.jobs-filter-search__input-wrap').find('input').val('');
+        $(this).hide();
+        $(this).closest('.jobs-filter-search__input-wrap').find('input').val('');
 
         liveSearch('');
-
-
     });
-
-
-
 
     // APPLY FORM  validation
 
     $('.apply-form input').on('input', function () {
-
         if ($(this).val() !== '') {
             $(this).closest('.user-data__field').find('.apply-label').hide();
-        }else{
+        } else {
             $(this).closest('.user-data__field').find('.apply-label').show();
-
         }
     });
-
 
     $('.apply-form .attach').on('click', function () {
         $(this).closest('.file__item-main').find('.file__item-upload').toggleClass('active');
@@ -257,11 +220,11 @@ jQuery(document).ready(function ($) {
 
     $('.apply-form .agreement').on('click', function (e) {
         e.preventDefault();
-        let agreementLabel = $(this).find('label'),
+
+        var agreementLabel = $(this).find('label'),
             agreementCheckbox = $(this).find('input');
 
-        agreementCheckbox.prop("checked", !agreementCheckbox.prop("checked"));
+        agreementCheckbox.prop('checked', !agreementCheckbox.prop('checked'));
         agreementLabel.toggleClass('checked');
-
     });
 });
